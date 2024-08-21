77b7d26d370aca2618f32dd85902f754b0978f0f
dnguyen316 <duonglethai.nguyen2@dxc.com>
2024-08-21 16:14:13 +0700

    chore: update some more file

---
+++ src/button.js
@@ -0,0 +1,10 @@
+//take a str, the button label and return a element
+
+/**
+ *
+ * @param {string} buttonName
+ * @returns {Element}
+ */
+module.exports = (buttonName) => {
+  return `Button: ${buttonName}`;
+};
